7091954ad63c787fbd248b0f75e24a62e26ea238
XelNagah <XelNagah@users.noreply.github.com>
2026-05-22 01:17:02 -0300

    chore: anonymize copyright + drop local-only fallback path

--- OneHandPractice/Properties/AssemblyInfo.cs
+++ OneHandPractice/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("OneHandPractice")]
 [assembly: AssemblyDescription("Filter beatmap notes to a single hand for technique practice in Beat Saber.")]
-[assembly: AssemblyCompany("jllarens")]
+[assembly: AssemblyCompany("XelNagah")]
 [assembly: AssemblyProduct("OneHandPractice")]
-[assembly: AssemblyCopyright("Copyright © 2026 Javier E. Rodríguez Llarens")]
+[assembly: AssemblyCopyright("Copyright © 2026 XelNagah")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
